b5fb83e9b0fbe65358340d114fbc5f1a5c65e7cf
Nhat Dao <ndao@stumail.jccc.edu>
2023-08-27 14:39:06 -0500

    Finished Lab 2

--- CS 250/Lab 2 - Dynamic Arrays/main.cpp
+++ CS 250/Lab 2 - Dynamic Arrays/main.cpp
@@ -44,6 +44,20 @@ void displayStackArray1D(int array[], int size);
 */
 void displayStackArray2D(int array[][ARRAY_SIZE], int rows, int cols);
 
+// ---------- LAB FOLLOWING -------------------
+// 1D FUNCTIONS
+int* createHeapArray1D(int size);
+void fillHeapArray1D(int* array, int size);
+void displayHeapArray1D(int* array, int size);
+void deleteHeapArray1D(int* array);
+
+// 2D FUNCTIONS
+int** createHeapArray2D(int row, int col);
+void fillHeapArray2D(int** array, int rows, int cols);
+void displayHeapArray2D(int** array, int rows, int cols);
+void deleteHeapArray2D(int** array, int rows);
+// ---------- LAB FOLLOWING END ---------------
+
 int main()
 {
 
@@ -61,21 +75,115 @@ int main()
 	displayStackArray2D(stackArray2D, ARRAY_SIZE, ARRAY_SIZE);
 
 	cout << endl << "1D HEAP ARRAY" << endl;
-	//heapArray1D = createHeapArray1D(ARRAY_SIZE);
-	//fillHeapArray1D(heapArray1D, ARRAY_SIZE);
-	//displayHeapArray1D(heapArray1D, ARRAY_SIZE);
-	//deleteHeapArray1D(heapArray1D);
+	// UNCOMMENTED FUNCTIONS
+	heapArray1D = createHeapArray1D(ARRAY_SIZE);
+	fillHeapArray1D(heapArray1D, ARRAY_SIZE);
+	displayHeapArray1D(heapArray1D, ARRAY_SIZE);
+	deleteHeapArray1D(heapArray1D);
+	// END UNCOMMENTED FUNCTIONS
 	heapArray1D = nullptr;
 	
 	cout << endl << endl << "2D HEAP ARRAY" << endl;
-	//heapArray2D = createHeapArray2D(ARRAY_SIZE, ARRAY_SIZE);
-	//fillHeapArray2D(heapArray2D, ARRAY_SIZE, ARRAY_SIZE);
-	//displayHeapArray2D(heapArray2D, ARRAY_SIZE, ARRAY_SIZE);
-	//deleteHeapArray2D(heapArray2D, ARRAY_SIZE);
+	// UNCOMMENTED FUNCTIONS
+	heapArray2D = createHeapArray2D(ARRAY_SIZE, ARRAY_SIZE);
+	fillHeapArray2D(heapArray2D, ARRAY_SIZE, ARRAY_SIZE);
+	displayHeapArray2D(heapArray2D, ARRAY_SIZE, ARRAY_SIZE);
+	deleteHeapArray2D(heapArray2D, ARRAY_SIZE);
+	// END UNCOMMENTED FUNCTIONS
 	heapArray2D = nullptr;
 	
 	return 0;
 }
+
+// ---------- LAB FOLLOWING -------------------
+// 2D FUNCTIONS
+void deleteHeapArray2D(int** array, int rows) {
+	if (array != nullptr) {
+		for (int i = 0; i < rows; i++) {
+			delete [] array[i];
+		}
+
+		delete [] array;
+	}
+}
+
+void displayHeapArray2D(int** array, int rows, int cols)
+{
+
+	if (array != nullptr) {
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				cout << array[i][j] << " ";
+			}
+
+			cout << endl;
+		}
+	}
+
+}
+
+int** createHeapArray2D(int row, int col) {
+	int** array = new int*[row];
+
+	for (int i = 0; i < row; i++) {
+		array[i] = new int[col];
+	}
+
+	return array;
+}
+
+void fillHeapArray2D(int** array, int rows, int cols)
+{
+	srand(static_cast<unsigned int>(time(0))); // seed random
+
+	if (array != nullptr) {
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < cols; j++)
+			{
+				array[i][j] = rand() % 10; // random int from 0 - 9
+			}
+		}
+	}
+
+}
+// 2D FUNCTIONS END
+
+// 1D FUNCTIONS
+void deleteHeapArray1D(int* array) {
+	delete [] array;
+}
+
+void displayHeapArray1D(int* array, int size)
+{
+	if (array != nullptr) {
+		for (int i = 0; i < size; i++)
+		{
+			cout << array[i] << " ";
+		}
+	}
+}
+
+int* createHeapArray1D(int size) {
+	return new int[size];
+}
+
+void fillHeapArray1D(int* array, int size)
+{
+	srand(static_cast<unsigned int>(time(0))); // seed random
+
+	if (array != nullptr) {
+		for (int i = 0; i < size; i++)
+		{
+			array[i] = rand() % 10; // random int from 0 - 9
+		}
+	}
+
+}
+// 1D FUNCTIONS END
+// ---------- LAB FOLLOWING END ---------------
 
 void fillStackArray1D(int array[], int size)
 {
