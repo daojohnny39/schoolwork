0e43ed7442d4f379aef7654746d9a1e20cd86ddf
Johnny Dao <johnnydao@johnnys-mbp-2.lan>
2023-11-16 11:49:43 -0600

    Made progress on CIS 275 Lab

--- CIS 275/Lab 7 - Templates/mytravel/mytravel/urls.py
+++ CIS 275/Lab 7 - Templates/mytravel/mytravel/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('homepages.urls')),
     path('admin/', admin.site.urls),
     path('showtrips/', include('travelsites.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
