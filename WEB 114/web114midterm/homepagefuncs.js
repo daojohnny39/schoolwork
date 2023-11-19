8adcd7adc6f5de61bb5f07ab5a36a2ba145824ac
ndao1JCCC <ndao1@stumail.jccc.edu>
2023-11-18 22:03:57 -0600

    adding unnecessary files

--- WEB 114/web114midterm/homepagefuncs.js
+++ WEB 114/web114midterm/homepagefuncs.js
@@ -4,37 +4,88 @@
     11/19/2023
 
     Project Requirements:
+        Numbers within "()" are references within the code.
+
+        Modify a web page's content by using the JavaScript textContent property to send information back to the user.  
+        Do not use window.alert() to send information to the user.
+            - (1) Use of replacing textContent
+
         1. How JavaScript expressions evaluate user data using +, -, *, /,  %, and **
-            - (1) Use of the * operator: Line 34
+            - (2) Use of the * operator
 */
 
 window.onload = function() {
     var cpuButton = document.querySelector('#CPU');
     cpuButton.addEventListener('click', addOnCart);
 
+    var gpuButton = document.querySelector('#GPU');
+    gpuButton.addEventListener('click', addOnCart);
+
+    /* ----------------- HIDDEN CHECKOUT DISPLAY ----------------- */
+    var popUpElement = document.getElementById("pop-up");
+
+    var span = document.getElementsByClassName("close")[0];
+
+    var checkoutButton = document.getElementById("checkout-button");
+    checkoutButton.onclick = function() {
+        popUpElement.style.display = "block";
+        generateCheckoutItems();
+    }
+
+    span.onclick = function() {
+        popUpElement.style.display = "none";
+    }
 }
 
 /* ----------------- CART COUNTERS ----------------- */
 var cartCounter = 0; 
 var totalPrice = 0;
+var cartItems = {};
 /* ----------------- CART COUNTERS END ----------------- */
 
 
 /* ----------------- CART FUNCTIONS ----------------- */
-function addOnCart() {
-    console.log("addOnCart was ran");
-    cartCounter++; // Increment the cartCounter variable
+function addOnCart(event) {
+
+    var productDiv = event.target.parentNode;
+
+    var quantity = productDiv.querySelector('select').value;
+    cartCounter += Number(quantity);
 
     var cartCount = document.getElementById('cart-counter');
-    cartCount.textContent = cartCounter;
+    cartCount.textContent = cartCounter;    // (1)
 
-    var quantity = document.querySelector('.product select').value;
-    var price =  document.querySelector('.product .price').textContent;
-    price = Number(price.replace('$', '')); // converting price to number
+    var price =  productDiv.querySelector('.price').textContent;    // (1)
+    price = Number(price.replace('$', ''));
 
-    // (1) Use of the * operator
-    totalPrice += price * quantity; 
+    totalPrice += price * quantity; // (2)
 
     var totalPriceElement = document.getElementById('total-price');
-    totalPriceElement.textContent = '$' + totalPrice;
+    totalPriceElement.textContent = '$' + totalPrice;   // (1)
+
+    var itemName = event.target.id; 
+
+    if (!cartItems[itemName]) {
+        cartItems[itemName] = Number(quantity);
+    }
+    else {
+        cartItems[itemName] += Number(quantity);
+    }
+
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+}
+/* ----------------- CART FUNCTIONS END ----------------- */
+
+/* ----------------- CHECKOUT FUNCTIONS ----------------- */
+function generateCheckoutItems() {
+    var checkoutItemsElement = document.getElementById('checkout-item');
+
+    var cartItems = JSON.parse(localStorage.getItem('cartItems'));
+
+    for (var key in cartItems) {
+        var itemElement = document.createElement('div');
+        itemElement.classList.add('each-item');
+        itemElement.textContent = key + ': ' + cartItems[key];  // (1)
+        checkoutItemsElement.appendChild(itemElement);
+    }
 }
